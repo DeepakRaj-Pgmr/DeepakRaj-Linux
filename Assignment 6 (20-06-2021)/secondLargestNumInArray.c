9753c48da1a2c6c52f83c62a1110d7bd494da33f
DeepakRaj-Pgmr <85957221+DeepakRaj-Pgmr@users.noreply.github.com>
2021-06-27 20:41:49 +0530

    Update secondLargestNumInArray.c

--- Assignment 6 (20-06-2021)/secondLargestNumInArray.c
+++ Assignment 6 (20-06-2021)/secondLargestNumInArray.c
@@ -7,24 +7,26 @@
 int main()
 {
    int numList[ARRAY_SIZE];
-   int i=0,firstLargest,secoundLargest;
+   int i=0,firstLargest,secondLargest = 0;
    printf("Enter the %d array element\n",ARRAY_SIZE);
    for(i=0;i<ARRAY_SIZE;i++)
    {
        scanf("%d",&numList[i]);
    }
    firstLargest=numList[0];
-   secoundLargest=numList[0];
-   for(i=0;i<ARRAY_SIZE;i++)
+   for(i=1;i<ARRAY_SIZE;i++)
    {
        if( numList[i] > firstLargest ){
             firstLargest=numList[i];
        }
-       if( numList[i] < firstLargest && numList[i] > secoundLargest ){
-            secoundLargest = numList[i];
+   }
+   for(i=0;i<ARRAY_SIZE;i++)
+   {
+	   if( numList[i] > secondLargest && numList[i] < firstLargest){
+            secondLargest = numList[i];
        }
    }
-   printf("Second largest number is : %d",secoundLargest);
+   printf("Second largest number is : %d",secondLargest);
 }
 /*
 Output:
@@ -32,12 +34,12 @@ Output:
 Test case 1:
 
 Enter the 5 array element
-5
-6
-1
-3
-2
-Second largest number is : 5
+80
+20
+80
+30
+40
+Second largest number is : 40
 
 ------------------------------------
 Test case 2:
